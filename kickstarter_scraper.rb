e24953ffaab080056e2234ecd5efb818b5c2deb7
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-03 18:11:06 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,10 +17,10 @@ def create_project_hash
 
    kickstarter.css("li.project.grid_4").each {|p|
      projects[p.css("h2.bbcard_name strong a").text.to_sym] = {
-         :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-         :description => project.css("p.bbcard_blurb").text,
-         :location => project.css("ul.project-meta span.location-name").text,
-         :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+         :image_link => p.css("div.project-thumbnail a img").attribute("src").value,
+         :description => p.css("p.bbcard_blurb").text,
+         :location => p.css("ul.project-meta span.location-name").text,
+         :percent_funded => p.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
        }
    }
 
